022a0a1fdeb1ddde762f593865070286412cfbc5
NAVEENA AM <naveena.am95@gmail.com>
2021-04-10 18:51:56 +0530

    initial

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,54 @@
+﻿using DataBase.Model;
+using MediatR;
+using MeditorDefinations.Queries;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RestServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public PersonController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+        // GET: api/<PersonController>
+        [HttpGet]
+        public async Task<List<Person>> Get()
+        {
+            return await mediator.Send(new GetPersonListQuery());
+        }
+
+        // GET api/<PersonController>/5
+        [HttpGet("{id}")]
+        public async Task<Person> Get(int id)
+        {
+            return await mediator.Send(new GetPersonByIdQuery(id));
+        }
+
+        // POST api/<PersonController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<PersonController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PersonController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
